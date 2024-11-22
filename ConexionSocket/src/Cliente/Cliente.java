15c585bf0b38435b9dcb8825ed57be159f9572e7
OwerAndres <owerandres611@gmail.com>
2024-11-22 13:06:05 +0100

    Implementacion cliente, (ya se reciben los mensajes)

--- ConexionSocket/src/Cliente/Cliente.java
+++ ConexionSocket/src/Cliente/Cliente.java
@@ -1,33 +1,157 @@
 package Cliente;
 
 import java.io.*;
+
 import java.net.*;
+
+import javax.imageio.IIOException;
+
+import javax.swing.JOptionPane;
 
 public class Cliente {
 
-    public static void main(String[] args) {
-        try (Socket socket = new Socket("172.16.40.172", 5555);
-             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader consola = new BufferedReader(new InputStreamReader(System.in))) {
+	static Socket socket;
 
-            System.out.println("Conectado al servidor.");
+	static BufferedReader reader, consola;
 
-            while (true) {
-                // Leer mensaje del servidor
-                String mensajeServidor = reader.readLine();
-                System.out.println(mensajeServidor);
+	static PrintWriter writer;
 
-                // Si el servidor espera entrada, enviarla
-                if (mensajeServidor.contains("escribe un mensaje")) {
-                    String mensajeCliente = consola.readLine();
-                    writer.println(mensajeCliente);
-                }
-            }
+	static String usuario, Contraseña, Contraseñadenuevo, mensajeServidor;
 
-        } catch (IOException e) {
-            System.out.println("Error en el cliente: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+
+		try {
+
+			socket = new Socket("172.16.41.149", 5555);
+
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			writer = new PrintWriter(socket.getOutputStream(), true);
+
+			consola = new BufferedReader(new InputStreamReader(System.in));
+
+			System.out.println("Conectando al servidor 5555");
+			while (true) {
+				String[] inicio = { "Inicio Sesión", "Registrarse" };
+				int seleccion = JOptionPane.showOptionDialog(null, "Selecciona una opción:", "Menú de opciones",
+						JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, inicio, inicio[0]);
+				switch (seleccion) {
+				case 0 -> {
+					InicioSesion();
+				}
+				case 1 -> {
+					Registrarse();
+				}
+				default -> {
+					JOptionPane.showMessageDialog(null, "No seleccionaste ninguna opción.", "Información",
+							JOptionPane.WARNING_MESSAGE);
+				}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Inicio Sesion
+	public static void InicioSesion() {
+		try {
+			writer.println("Inicio Sesion");
+			mensajeServidor = reader.readLine();
+			if (mensajeServidor.contains("Introduce el nombre de usuario")) {
+				usuario = JOptionPane.showInputDialog(null, "Nombre de usuario", "Entrada de texto",
+						JOptionPane.QUESTION_MESSAGE);
+				writer.println(usuario);
+				mensajeServidor = reader.readLine();
+
+				if (mensajeServidor.contains("Introduce la contraseña")) {
+					Contraseña = JOptionPane.showInputDialog(null, "Contraseña", "Entrada de texto",
+							JOptionPane.QUESTION_MESSAGE);
+					writer.println(Contraseña);
+
+				}
+				mensajeServidor = reader.readLine();
+
+				if (mensajeServidor.contains("Contraseña Correcta")) {
+					JOptionPane.showMessageDialog(null, "Contraseña correcta");
+					Seleccion();
+
+				} else if (mensajeServidor.contains("Contraseña Incorrecta")) {
+					JOptionPane.showMessageDialog(null, "Contraseña incorrecta intentelo de nuevo", "",
+							JOptionPane.WARNING_MESSAGE, null);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Registrarse
+	public static void Registrarse() {
+
+		try {
+			writer.println("Registrarse");
+			mensajeServidor = reader.readLine();
+
+			usuario = JOptionPane.showInputDialog(null, "Nombre de usuario", "Entrada de texto",
+
+					JOptionPane.QUESTION_MESSAGE);
+
+			Contraseña = JOptionPane.showInputDialog(null, "Introduce Contraseña", "Entrada de texto",
+
+					JOptionPane.QUESTION_MESSAGE);
+
+			Contraseñadenuevo = JOptionPane.showInputDialog(null, "Introduce de vuelta la contraseña",
+					"Entrada de texto", JOptionPane.QUESTION_MESSAGE);
+
+			if (Contraseña.equals(Contraseñadenuevo)) {
+
+				writer.println("Usuario: "+usuario);
+
+				writer.println("Contraseña: "+Contraseña);
+
+			} else {
+
+				JOptionPane.showMessageDialog(null, "Contraseña Incorrecta", "Advertencia",
+						JOptionPane.WARNING_MESSAGE);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Seleccion
+	public static void Seleccion() {
+		String[] Modos = { "Multijugador", "Contra la maquina", "Salir" };
+		boolean exit = true;
+		do {
+			int seleccion = JOptionPane.showOptionDialog(null, "Selecciona una opción:", "Menú de opciones",
+					JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, Modos, Modos[0]);
+			switch (seleccion) {
+			case 0 -> {
+				Multijugador();
+			}
+			case 1 -> {
+				Maquina();
+			}
+			case 2 -> {
+				exit = false;
+			}
+			default -> {
+				JOptionPane.showMessageDialog(null, "No seleccionaste ninguna opción.", "Información",
+						JOptionPane.WARNING_MESSAGE);
+			}
+
+			}
+
+		} while (exit);
+	}
+
+	public static void Maquina() {
+		boolean exitlocal = true;
+	}
+
+	public static void Multijugador() {
+	}
+
 }
--- ConexionSocket/src/conexion/Conexion.java
+++ ConexionSocket/src/conexion/Conexion.java
@@ -39,41 +39,41 @@ public class Conexion {
 					BufferedReader reader2 = new BufferedReader(new InputStreamReader(cliente2.getInputStream()));
 					PrintWriter writer2 = new PrintWriter(cliente2.getOutputStream(), true)) {
 				int contador = 0;
-				while (contador < 3) {
-//                    // Cliente 1 envía mensaje, Cliente 2 recibe
-//                    writer1.println("Es tu turno, escribe un mensaje para el Cliente 2:");
-//                    String mensaje1 = reader1.readLine();
-//                    System.out.println("Cliente 1 dice: " + mensaje1);
-//                    
+				while (true) {
+                    // Cliente 1 envía mensaje, Cliente 2 recibe
+                    writer1.println("Es tu turno, escribe un mensaje para el Cliente 2:");
+                    String mensaje1 = reader1.readLine();
+                    System.out.println("Cliente 1 dice: " + mensaje1);
+                    
+
+                    // Cliente 2 envía mensaje, Cliente 1 recibe
+                    writer2.println("Es tu turno, escribe un mensaje para el Cliente 1:");
+                    String mensaje2 = reader2.readLine();
+                    System.out.println("Cliente 2 dice: " + mensaje2);
+
+//					// Obtener las opciones seleccionadas por los jugadores
+//					writer1.println("Es tu turno, escribe un mensaje para el Cliente 2:");
+//					String[] opciones = { "Piedra", "Papel", "Tijera" };
+//					int panel = JOptionPane.showOptionDialog(null, "Elige una opcion", "jugador 1", 0, 1, null, opciones,
+//							opciones);
+//					int opcPLayed1 = panel;
 //
-//                    // Cliente 2 envía mensaje, Cliente 1 recibe
-//                    writer2.println("Es tu turno, escribe un mensaje para el Cliente 1:");
-//                    String mensaje2 = reader2.readLine();
-//                    System.out.println("Cliente 2 dice: " + mensaje2);
-
-					// Obtener las opciones seleccionadas por los jugadores
-					writer1.println("Es tu turno, escribe un mensaje para el Cliente 2:");
-					String[] opciones = { "Piedra", "Papel", "Tijera" };
-					int panel = JOptionPane.showOptionDialog(null, "Elige una opcion", "jugador 1", 0, 1, null, opciones,
-							opciones);
-					int opcPLayed1 = panel;
-
-					writer2.println("Es tu turno, escribe un mensaje para el Cliente 1:");
-					String[] opciones2 = { "Piedra", "Papel", "Tijera" };
-					int panel2 = JOptionPane.showOptionDialog(null, "Elige una opcion", "jugador 2", 0, 1, null,
-							opciones2, opciones2);
-					int opcPlayed2 = panel2;
-
-					if (opcPLayed1 != 100 && opcPlayed2 != 100) {
-						metodos.game(opcPLayed1, opcPlayed2);
-						contador++;
-					}
-
+//					writer2.println("Es tu turno, escribe un mensaje para el Cliente 1:");
+//					String[] opciones2 = { "Piedra", "Papel", "Tijera" };
+//					int panel2 = JOptionPane.showOptionDialog(null, "Elige una opcion", "jugador 2", 0, 1, null,
+//							opciones2, opciones2);
+//					int opcPlayed2 = panel2;
+//
+//					if (opcPLayed1 != 100 && opcPlayed2 != 100) {
+//						metodos.game(opcPLayed1, opcPlayed2);
+//						contador++;
+//					}
+//
+//				}
+//				if(contador == 3) {
+//					metodos.ganador();
+//				}
 				}
-				if(contador == 3) {
-					metodos.ganador();
-				}
-
 			} catch (IOException e) {
 				System.out.println("Error en la comunicación entre clientes: " + e.getMessage());
 			}
